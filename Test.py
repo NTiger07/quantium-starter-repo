4073dda98fdea4b6ae07e73d891d680441511932
NTiger07 <falaleru@gmail.com>
2022-07-17 20:27:16 +0100

    done

--- Test.py
+++ Test.py
@@ -1,40 +1,33 @@
-# print("Hello world")
-import csv
 from pandas import *
+#read files
+data1 = read_csv("./data/daily_sales_data_0.csv")
+data2 = read_csv("./data/daily_sales_data_1.csv")
+data3 = read_csv("./data/daily_sales_data_2.csv")
 
+#filter pink morsel products
+data1 = data1[data1['product'].str.contains('pink morsel')]
+data2 = data2[data2['product'].str.contains('pink morsel')]
+data3 = data3[data3['product'].str.contains('pink morsel')]
 
+#filtered products
+data1.to_csv("./data1.csv")
+data2.to_csv("./data2.csv")
+data3.to_csv("./data3.csv")
 
-# # price = data['price']
-# data1 = read_csv("./data/daily_sales_data_0.csv")
-# data2 = read_csv("./data/daily_sales_data_1.csv")
-# data3 = read_csv("./data/daily_sales_data_2.csv")
-
-# data1 = data1[data1['product'].str.contains('pink morsel')]
-# data2 = data2[data2['product'].str.contains('pink morsel')]
-# data3 = data3[data3['product'].str.contains('pink morsel')]
-
-# data1.to_csv("./data1.csv")
-# data2.to_csv("./data2.csv")
-# data3.to_csv("./data3.csv")
-
-# total = concat(map(read_csv, ['./data1.csv', './data2.csv', './data3.csv']))
-# trim = total.drop(total.columns[[0,1]], axis=1)
-# trim.to_csv('totalData.csv')
+#join files
+total = concat(map(read_csv, ['./data1.csv', './data2.csv', './data3.csv']))
+trim = total.drop(total.columns[[0]], axis=1)
+trim.to_csv('totalData.csv')
 data = read_csv("./totalData.csv")
 
-
+#needed data
 price =data['price'].str.replace('$', '').astype(float)
 quantity = data['quantity'].astype(float)
-
+product = data['product']
 sales = (price * quantity).tolist()
 date = data['date'].tolist()
 region = data['region'].tolist()
 
-print(sales)
-
-
-
-
-
-
-
+#final output
+df = DataFrame({'product': product, 'sales($)': sales, 'date': date, 'region': region} )
+df.to_csv("./complete.csv")
