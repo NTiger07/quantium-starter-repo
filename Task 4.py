214b32d6bec1d94cf09b65ceee29ee21311acc82
NTiger07 <falaleru@gmail.com>
2022-07-22 15:42:55 +0100

    done

--- Task 4.py
+++ Task 4.py
@@ -1,8 +1,9 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 from pandas import DataFrame, read_csv
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 data = read_csv('./complete.csv')
 date = data['date']
@@ -37,18 +38,22 @@ def update_line_chart(value):
     if value == 'North':
         fig = px.line(north, 
         x = north_date, y=north).update_layout(xaxis_title="Date", yaxis_title="Sales($) at North")
+        fig.update_traces(line_color = '#430802')
         return fig
     elif value == 'South':
         fig = px.line(south, 
         x = south_date, y=south).update_layout(xaxis_title="Date", yaxis_title="Sales($) at South")
+        fig.update_traces(line_color = '#C0D1EB')
         return fig
     elif value == 'East':
         fig = px.line(east, 
         x = east_date, y=east).update_layout(xaxis_title="Date", yaxis_title="Sales($) at East")
+        fig.update_traces(line_color = '#5F3509')
         return fig
     elif value == 'West':
         fig = px.line(west, 
         x = west_date, y=west).update_layout(xaxis_title="Date", yaxis_title="Sales($) at West")
+        fig.update_traces(line_color = 'black')
         return fig
     else:
         fig = px.line(df[data['region'].str.contains('south')], 
