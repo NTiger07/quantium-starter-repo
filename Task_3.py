bb3ad0d288c385b49c0a206c077f5e5c4b8a2ff5
NTiger07 <falaleru@gmail.com>
2022-07-28 13:22:12 +0100

    task_3 fix

--- Task_3.py
+++ Task_3.py
@@ -1,5 +1,9 @@
 from pandas import read_csv
 import plotly.express as px
+from dash import Dash, html, dcc
+
+#instantiate app
+app = Dash(__name__)
 
 #read data from file
 data = read_csv('./complete.csv')
@@ -7,12 +11,12 @@ date = data['date']
 sales = data['sales($)']
 region = data['region']
 
-#optional sorting
-north = data[data['region'].str.contains('north')]
-south = data[data['region'].str.contains('south')]
-east = data[data['region'].str.contains('east')]
-west = data[data['region'].str.contains('west')]
-
-#display data on line_graph.html
+#line graph
 fig = px.line(x = date, y = sales, title = 'Total sales($) of pink morsel by Soul Foods').update_layout(xaxis_title="Date", yaxis_title="Sales($)")
-fig.write_html('line_graph.html', auto_open=True)
+
+#app layout
+app.layout = html.Div([
+    dcc.Graph(id="graph", figure = fig)
+])
+
+app.run_server(debug=True)
